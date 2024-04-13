b05d7c69484667a36e94a1a883789b62a25cac40
matteokalcich <matteokalcich@gmail.com>
2024-04-13 16:59:29 +0200

    Scelta gioco

--- src/Client.java
+++ src/Client.java
@@ -1,28 +1,29 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         try {
+
+            Scanner scanner = new Scanner(System.in);
             // Creazione della connessione al server sulla porta 9999
-            Socket socket = new Socket("localhost", 9999);
+            Socket socket = new Socket("192.168.52.27", 9999);
 
             // Apertura dei flussi di input e output per la comunicazione con il server
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-            // Invio di una stringa al server
-            String message = "Ciao server!";
+            // Ricezione e stampa della risposta dal server
+            String text = in.readLine();
+            System.out.println(text);
+            String message = scanner.nextLine();
             out.println(message);
 
-            // Ricezione e stampa della risposta dal server
-            String response = in.readLine();
-            System.out.println("Risposta dal server: " + response);
+
 
             // Chiusura delle risorse
-            in.close();
-            out.close();
-            socket.close();
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
